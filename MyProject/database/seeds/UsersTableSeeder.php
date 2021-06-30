0f9d6e88655fcca108464958994712e30b6b1c83
FrancescoPierfederici <francescopierf@gmail.com>
2021-06-30 18:39:27 +0200

    commit

--- MyProject/database/seeds/UsersTableSeeder.php
+++ MyProject/database/seeds/UsersTableSeeder.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\comic;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -12,5 +12,16 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+        $comicsList=config("comics");
+        // con questo metodo ho la lista dei miei comics
+        foreach ( $comicsList as $comics){
+            $comicsEntry = new Comic();
+            // $comicsEntry->title=$comics
+            $comicsEntry->fill($comics);
+            // dopo aver costruito la struttura fillable sul model che sta sul provider (comics.php)
+            // vado a riempire i dati e a salvarli subito dentro al mio database
+
+            $comicsEntry->save();
+        }
     }
 }
